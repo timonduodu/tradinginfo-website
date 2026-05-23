029b0dab41de24aa9d844db5633be977a0ed547e
timonduodu <timon.duodu@outlook.com>
2026-05-23 20:14:58 +0200

    Sitemap-Integration + Domain-Fix tradingwissen.ch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-const SITE_URL = process.env.SITE_URL || 'https://tradinginfo.ch';
+import sitemap from '@astrojs/sitemap';
+
+const SITE_URL = process.env.SITE_URL || 'https://tradingwissen.ch';
 
 export default defineConfig({
   site: SITE_URL,
+
   build: {
     format: 'directory',
   },
+
+  integrations: [sitemap()],
 });
